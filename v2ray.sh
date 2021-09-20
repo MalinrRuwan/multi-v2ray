4eea227df93ca4269aea8b70059f84435af97c76
MalinrRuwan <78165837+MalinrRuwan@users.noreply.github.com>
2021-09-20 10:10:24 +0530

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -162,7 +162,7 @@ installDependent(){
 updateProject() {
     [[ ! $(type pip 2>/dev/null) ]] && colorEcho $RED "pip no install!" && exit 1
 
-    pip install -U v2ray_util
+    pip install -U git+https://github.com/MalinrRuwan/multi-v2ray#egg=v2ray_util
 
     if [[ -e $UTIL_PATH ]];then
         [[ -z $(cat $UTIL_PATH|grep lang) ]] && echo "lang=en" >> $UTIL_PATH
